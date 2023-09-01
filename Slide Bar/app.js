50cb9627054771d76f285eb965d8367bbd3fdf74
MUHAMMAD HUZAIFA JAWED <malik2024huzaifa@gmail.com>
2023-09-01 20:33:07 +0500

    Update app.js

--- Slide Bar/app.js
+++ Slide Bar/app.js
@@ -2,8 +2,15 @@
 
 let nav_icon = document.getElementById('nav-icon');
 let nav_items = document.getElementById('nav-items');
+let cross_icon = document.getElementById('cross-icon');
 
 nav_icon.addEventListener('click' , function () {
     nav_items.classList.toggle('show-nav-items');
+});
+
+// When User Click on Cross-icon Hide Slide-Bar
+cross_icon.addEventListener('click', function()
+{
+    nav_items.classList.remove('show-nav-items')
 });
 
